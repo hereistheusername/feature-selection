6be443335cdd01d50e1585410bb617eae2456782
xinglan <Xinglan_Liu@foxmail.com>
2021-03-11 18:00:15 +0800

    Test fixed cross validation on special datasets

--- test/fakecrossvalidatontest.py
+++ test/fakecrossvalidatontest.py
@@ -6,10 +6,11 @@ from src.datareader import read_data
 class FakeCrossValidationTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(sel):
-        sel.data_path = 'data/fivelines.txt'
+        sel.data_path = 'data/CS170_small_special_testdata__95.txt'
 
     def test_range(self):
         data = read_data(self.data_path)
-        accuracy = leave_one_out_cross_validation(data,{1,3,4},2)
+        accuracy = leave_one_out_cross_validation(data,{7,4},6)
         self.assertLessEqual(accuracy, 1)
+        self.assertEqual(accuracy, 0.92)
         self.assertGreaterEqual(accuracy, 0)
